a3a25ddc740fcc66b6e2f6d429c33a4d3932d270
Ishank Saxena <ish.sxn@gmail.com>
2019-05-23 02:17:00 +0530

    11th Lok Sabha Crawled

--- Parliament/parliament/parliament/spiders/ls_all_member_more_details.py
+++ Parliament/parliament/parliament/spiders/ls_all_member_more_details.py
@@ -115,7 +115,7 @@ class Ls12MemberMoreDetailsSpider(scrapy.Spider):
         if "Date of Birth" in details:
             member_details["DOB"] = details[details.index("Date of Birth")+2]
         if "Place of Birth" in details:
-            member_details["Birth_place"] = details[details.index("Date of Birth")+2]
+            member_details["Birth_place"] = details[details.index("Place of Birth")+2]
         if "Marital Status" in details:
             member_details["Marital_Status"] = details[details.index("Marital Status")+2].split()[0]
         if "Children" in details:
@@ -139,3 +139,62 @@ class Ls12MemberMoreDetailsSpider(scrapy.Spider):
         self.collection.update_one({"ID":member_id},{"$set":member_details})
         image_url = response.url.replace("htm","jpg")
         yield ImageItem(image_urls = [image_url], image_name = response.request.meta["ID"])
+
+
+class Ls11MemberMoreDetailsSpider(scrapy.Spider):
+    name = 'ls_11_member_details'
+    config_file = open("config.json")
+    config = json.load(config_file)
+    client = pymongo.MongoClient(config["mongo_server"])
+    db = client["factly_parliament_search"]
+    collection = db["ls_all_member_urls"]
+    custom_settings = {"ITEM_PIPELINES": {'parliament.pipelines.CustomImageNamePipeline': 1},"IMAGES_STORE":"Images"}
+    def start_requests(self):
+        print("Starting")
+        compatible_members = self.collection.find(
+            {"$and": [
+                {"Sessions": {"$nin": ["12","13", "14", "15", "16"]}}, 
+                {"Sessions": "11"}]})
+        print("Results:",compatible_members.count())
+        for member in compatible_members:
+            url = member["URL"]
+            print(url)
+            yield scrapy.Request(url = url, callback=self.parse_profile, meta={"ID":member["ID"]})
+
+    def parse_profile(self,response):
+
+        member_id = response.request.meta["ID"]
+        member_details = {}
+        member_details["Name"] = response.css("font[size='5']::text").extract_first().title().strip()
+        
+        if len(response.css("font")) > 0:
+            details = list(response.css("font[size='4']")[1].css("::text").extract())
+            details = [_.strip() for _ in details]
+
+            if "Date of Birth" in details:
+                member_details["DOB"] = details[details.index("Date of Birth")+2]
+            if "Place of Birth" in details:
+                member_details["Birth_place"] = details[details.index("Place of Birth")+2]
+            if "Marital Status" in details:
+                member_details["Marital_Status"] = details[details.index("Marital Status")+2].split()[0]
+            if "Children" in details:
+                children_details = details[details.index("Children")+2].split()
+                children_details = [_.rstrip("s") for _ in children_details]
+                if "son" in children_details:
+                    sons = children_details[children_details.index("son") - 1]
+                    member_details["Sons"] = w2n.word_to_num(sons)
+                if "daughter" in children_details:
+                    daughters = children_details[children_details.index("daughter") - 1]
+                    member_details["Daughters"] = w2n.word_to_num(daughters)
+            if "Educational Qualifications" in details:
+                education = details[details.index("Educational Qualifications")+2]
+                education = education.replace("\r","").replace("\n","")
+                member_details["Education"] = education
+            if "Profession" in details:
+                member_details["Profession"] = details[details.index("Profession")+2]            
+
+                print(member_details)
+                
+            self.collection.update_one({"ID":member_id},{"$set":member_details})
+            image_url = response.url.replace("htm","gif")
+            yield ImageItem(image_urls = [image_url], image_name = response.request.meta["ID"])
